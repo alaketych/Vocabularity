2db0da16dfcf1524f307c685010719e9035d0b83
Oleksandr Laketych <oleksandr.laketych@dataart.com>
2024-05-01 20:34:11 +0300

    [BE] implement user service

---
+++ ConsoleApp1/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using Vocabularity.Core;
+
+namespace Vocabularity.Service.User.Entities;
+
+public class User : Entity
+{
+    [JsonPropertyName("user_login")]
+    public string UserLogin { get; set; }
+
+    [JsonPropertyName("user_pseudonym")]
+    public string Pseudonym { get; set; }
+
+    [JsonPropertyName("user_password")]
+    public string Password { get; set; }
+
+    [JsonIgnore]
+    public string PasswordSalt { get; set; }
+
+    public User() : base(true) 
+    { 
+    }
+}
---
+++ ConsoleApp1/Implementation/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Cosmos;
+using Vocabularity.Core;
+using Vocabularity.Core.Implementation;
+using Vocabularity.Service.User.Interfaces;
+
+namespace Vocabularity.Service.User.Implementation
+{
+    public class UserRepository : Repository<Entities.User>, IUserRepository
+    {
+        public readonly CosmosClient cosmosClient;
+        public readonly Container cosmosContainer;
+
+        public override string DatabaseId => throw new NotImplementedException();
+
+        public override string ContainerId => throw new NotImplementedException();
+
+        public UserRepository(CosmosClient cosmosClient) : base(cosmosClient)
+        {
+            this.cosmosClient = cosmosClient;
+            cosmosContainer = this.cosmosClient.GetContainer(DatabaseId, ContainerId);
+        }
+    }
+}
